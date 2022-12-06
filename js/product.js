7e22370c6da9597fb46f634925647de1e8f46572
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-12-06 16:01:05 +0200

    06/12

--- js/product.js
+++ js/product.js
@@ -33,34 +33,6 @@ this.salePrice = salePrice
 
 
 
-// background: linear-gradient(to right,#435e36,#323663);
-
-
-
-/* <form class="input-group" id="Reg" method="GET" action="page4.html">
-                   
-<input type="text" placeholder="*  שם משתמש" class="input-field"  required>
-<input type="password" placeholder="*  סיסמא" class="input-field"  required>
-<input type="password" placeholder="*  אימות סיסמא" class="input-field"  required>
-<input type="email" placeholder="* אימייל" class="input-field"  required>
-
-<input type="checkbox" name="remember"  class="check-box" ><span id="span1">קראתי והסכמתי לתנאי השימוש</span>
-<button type="submit" class="submit-Btn" > הירשם</button>
-</form> */
-
-
-
-// <div class="row">
-// <div class="col">
-//   <input type="text" class="form-control"  placeholder="* שם פרטי" required>
-// </div>
-// <div class="col">
-//   <input type="text" class="form-control" placeholder="*  שם משפחה" required>
-// </div>
-// </div>
-
-
-
 
 
 
